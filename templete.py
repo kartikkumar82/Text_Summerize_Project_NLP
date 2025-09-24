73969491d6245f7ea5228ce5891abcb55faa77a9
kartikkumar82 <kartikkumar1800089@gmail.com>
2025-09-24 11:47:35 +0530

    file structure

---
+++ templete.py
@@ -0,0 +1,49 @@
+import os
+from pathlib import Path  
+import logging
+
+# Configure logging to print timestamp and message
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
+
+project_name = 'TextSummarizer'
+
+# List of project files to create
+list_of_files = [
+    ".github/workflows/.gitkeep",
+    f"src/{project_name}/__init__.py",
+    f"src/{project_name}/components/__init__.py",   
+    f"src/{project_name}/utils/__init__.py",
+    f"src/{project_name}/utils/common.py",
+    f"src/{project_name}/logging/__init__.py",
+    f"src/{project_name}/config/__init__.py",
+    f"src/{project_name}/config/configuration.py",
+    f"src/{project_name}/pipeline/__init__.py",
+    f"src/{project_name}/entity/__init__.py",
+    f"src/{project_name}/constants/__init__.py",
+    "config/config.yaml",
+    "params.yaml",
+    "app.py",
+    "main.py",
+    "Dockerfile",
+    "requirements.txt",
+    "setup.py",
+    "research/trials.ipynb",
+]
+
+# Loop through each file path and create folder + file
+for filepath in list_of_files:
+    filepath = Path(filepath)   # Convert string path into a Path object
+    filedir, filename = os.path.split(filepath)
+
+    # Create directory if it doesn’t exist
+    if filedir != "":
+        os.makedirs(filedir, exist_ok=True)
+        logging.info(f"Creating directory: {filedir} for the file {filename}")
+
+    # Create empty file if it doesn’t exist OR file is empty
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+        with open(filepath, 'w') as f:
+            pass
+        logging.info(f"Creating empty file: {filepath}")
+    else:
+        logging.info(f"{filename} already exists")
